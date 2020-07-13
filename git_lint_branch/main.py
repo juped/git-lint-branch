76707ed09fa679f0960923f9cf15bb33139993e2
Raymond E. Pasco <ray@ameretat.dev>
2020-07-13 19:40:08 -0400

    Run single-commit linters on each commit

--- git_lint_branch/main.py
+++ git_lint_branch/main.py
@@ -1,6 +1,8 @@
 import typer
 from pygit2 import Repository
 from pygit2 import GIT_SORT_TOPOLOGICAL
+from git_lint_branch.linter_output import *
+from git_lint_branch.single import single_linters
 
 app = typer.Typer()
 
@@ -15,4 +17,8 @@ def main(upstream: str):
     walker = repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL)
     walker.hide(upstream.id)
     for commit in walker:
-        pass
+        for linter in single_linters:
+            lint_result = linter(commit)
+            if lint_result.level is not LinterLevel.Empty:
+                print('Commit: {id}'.format(id=commit.id))
+                lint_result.pretty_print()
