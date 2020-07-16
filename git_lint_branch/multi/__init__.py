6ce901110fbe87d0352d6d467ed07a936c8d96ee
Raymond E. Pasco <ray@ameretat.dev>
2020-07-16 15:13:17 -0400

    Add any_merges multi-linter

--- git_lint_branch/multi/__init__.py
+++ git_lint_branch/multi/__init__.py
@@ -1,3 +1,3 @@
 from git_lint_branch.multi.multi_linters import *
 
-multi_linters = [repeat_linter]
+multi_linters = [repeat_linter, any_merges]
--- git_lint_branch/multi/multi_linters.py
+++ git_lint_branch/multi/multi_linters.py
@@ -27,3 +27,29 @@ def repeat_linter(walker: Walker):
 
     result.message += '\n'
     return result
+
+def any_merges(walker: Walker):
+    result = LinterOutput()
+    result.level = LinterLevel.Empty
+
+    merge_list = []
+
+    for commit in walker:
+        if len(commit.parents) > 1:
+            merge_list.append(commit)
+
+    if len(merge_list) > 0:
+        result.level = LinterLevel.Caution
+        result.message = '''
+        This branch contains merge commits (commits with two or more parents).
+        Ordinarily, merge commits should only appear in upstream 'integration'
+        branches, not in topic branches. However, merge commits could be correct
+        if the topic was split into multiple 'subtopics' which were then merged
+        into the general topic.
+        '''
+        result.help_string = '''
+        Any `git rebase` operation, such as `git rebase master`, removes merges
+        from the commit history by default.
+        '''
+
+    return result
