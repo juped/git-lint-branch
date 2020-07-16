441a058d9603a963c1fa3ffe3bce3a44fe43a7b7
avik-pal <avikpal@iitk.ac.in>
2020-07-16 12:30:21 +0530

    Add tense linter which warns on using Past Tense

--- git_lint_branch/single/__init__.py
+++ git_lint_branch/single/__init__.py
@@ -2,8 +2,10 @@ from pygit2 import Commit
 from git_lint_branch.linter_output import *
 from git_lint_branch.single.linters import *
 from git_lint_branch.single.regex_linter import *
+from git_lint_branch.single.tense_linter import *
 
 single_linters = [
     example_linter,
     regex_linter,
+    tense_linter,
 ]
