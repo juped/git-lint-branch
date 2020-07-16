4315667957098b1ec97fd214a5bb774492dbf35a
Raymond E. Pasco <ray@ameretat.dev>
2020-07-16 15:00:42 -0400

    Use config file for regex linter

--- git_lint_branch/single/regex_linter.py
+++ git_lint_branch/single/regex_linter.py
@@ -2,27 +2,27 @@ import re
 
 from pygit2 import Commit
 from git_lint_branch.linter_output import *
-
-# TODO: Load from config
-REGEX = r'^(.+:\ )?[A-Z].+\.$'
-TITLE_LENGTH = 76
-BODY_LINE_LENGTH = 76
+import git_lint_branch.cfg as cfg
 
 def regex_linter(commit: Commit):
     result = LinterOutput()
+
+    regex = cfg.config.get('regex_linter', 'regex', fallback=r".*")
+    title_length = int(cfg.config.get('regex_linter', 'title_length', fallback=50))
+    body_line_length = int(cfg.config.get('regex_linter', 'body_line_length', fallback=72))
 
     result.level = LinterLevel.Empty
     result.title = 'Commit message format'
     result.message = '''
     '''
     result.help_string = f'''
-    Make sure the commit message title follows this pattern: {REGEX}
-    and is under {TITLE_LENGTH} characters.
+    Make sure the commit message title follows this pattern: {regex}
+    and is under {title_length} characters.
 
-    Also keep each line of the commit message body under {BODY_LINE_LENGTH} characters.
+    Also keep each line of the commit message body under {body_line_length} characters.
     '''
 
-    pattern = re.compile(REGEX, re.UNICODE)
+    pattern = re.compile(regex, re.UNICODE)
     commit_title = commit.message.splitlines()[0]
     if pattern.fullmatch(commit_title) is None:
         result.level = LinterLevel.Warning
@@ -30,14 +30,14 @@ def regex_linter(commit: Commit):
         '''Commit title does not match pattern.
     '''
 
-    if len(commit_title) > TITLE_LENGTH:
+    if len(commit_title) > title_length:
         result.level = max(result.level, LinterLevel.Warning)
         result.message += \
         '''Commit title too long.
     '''
 
     for line in commit.message.splitlines()[1:]:
-        if len(line) > BODY_LINE_LENGTH:
+        if len(line) > body_line_length:
             result.level = max(result.level, LinterLevel.Notice)
             result.message += \
             '''Commit body lines too long.
